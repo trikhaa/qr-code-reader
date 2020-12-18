d5bd8160e5680b3e2b1fb6e977cf71290b84a5eb
gouravrana41 <gouravrana41@gmail.com>
2020-12-18 13:09:51 +0530

    Android fixed orientation

---
+++ android/src/main/java/com/leapover/plugin/qrcode/PortraitActivity.java
@@ -0,0 +1,12 @@
+package com.leapover.plugin.qrcode;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class PortraitActivity extends CaptureActivity {
+}
