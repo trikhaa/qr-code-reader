71b30bfc739f9f07d2d8951d3106b3573735f9da
Ashish Trikha <52153399+trikhaa@users.noreply.github.com>
2020-12-24 19:44:59 +0530

    Added changes to remove red line and bottom status text

--- android/src/main/java/com/leapover/plugin/qrcode/PortraitActivity.java
+++ android/src/main/java/com/leapover/plugin/qrcode/PortraitActivity.java
@@ -1,12 +1,32 @@
 package com.leapover.plugin.qrcode;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-
-import androidx.annotation.Nullable;
+import android.view.View;
+import android.widget.TextView;
 
 import com.journeyapps.barcodescanner.CaptureActivity;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+import com.journeyapps.barcodescanner.ViewfinderView;
+import com.leapover.plugin.qrcode.qrcodeplugin.R;
 
-public class PortraitActivity extends CaptureActivity {
+public class PortraitActivity extends CaptureActivity implements DecoratedBarcodeView.TorchListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ViewfinderView viewfinderView = findViewById(R.id.zxing_viewfinder_view);
+        viewfinderView.setLaserVisibility(false);
+        TextView statusView = findViewById(R.id.zxing_status_view);
+        statusView.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onTorchOn() {
+
+    }
+
+    @Override
+    public void onTorchOff() {
+
+    }
 }
